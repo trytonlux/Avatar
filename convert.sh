e84f1326cdafe9460a30c0f3c1c373ce0166e432
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-07-12 01:27:15 -0400

    Added convert_svg function that runs rsvg-convert with given dimensions. Run convert_svg with sizes 512,256,128,64.

--- convert.sh
+++ convert.sh
@@ -8,4 +8,14 @@ check_rsvg()
     fi
 }
 
+convert_svg()
+{
+    # Converts avatar.svg to a PNG with given size
+    rsvg-convert --height="$1" --width="$1" --output "x$1.png" avatar.svg
+}
+
 check_rsvg
+convert_svg 512
+convert_svg 256
+convert_svg 128
+convert_svg 64
