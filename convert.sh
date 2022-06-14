0a7e31cdf0ed771db5be10ecb20b0edae0d184ef
Tryton Van Meer <hello@trytonvanmeer.dev>
2022-06-14 16:41:49 -0400

    remove function for checking rsvg is installed.

--- convert.sh
+++ convert.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 
-check_rsvg()
-{
-    # shellcheck disable=SC2039
-    if ! type "rsvg-convert" &> /dev/null; then
-        echo "missing dependency: librsvg"
-        exit
-    fi
-}
-
 convert_svg()
 {
     # Converts avatar.svg to a PNG with given size
-    rsvg-convert --height="$1" --width="$1" --output "x$1.png" avatar.svg
+    rsvg-convert --height="$1" --width="$1" --output "x${1}.png" avatar.svg
 }
 
-check_rsvg
 convert_svg 512
 convert_svg 256
 convert_svg 128
